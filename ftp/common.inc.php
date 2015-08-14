788f41537111210466714223eadd12691fe15c02
kevins1022 <1927613522@qq.com>
2015-08-14 11:43:01 +0800

    帮同事修改ecshop bug

---
+++ ftp/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'sgxdd';
+$cfg_dbuser = 'sgxdd';
+$cfg_dbpwd = 'liuliang889';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
